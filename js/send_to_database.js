783adbdbe70c86f7d19066c14ac1353bb94b5e34
Elvin Lin <ssfvessfve@163.com>
2017-08-16 21:56:00 +0800

    upload

---
+++ js/send_to_database.js
@@ -0,0 +1,57 @@
+
+var queryGameInfoEN = function(json) {
+    try { 
+    	//alert('hello')
+		nameEN = json.name
+		//imgCaption = '../img/'+nameEN+'/caption.jpg';
+		//alert(imgCaption)
+		//$('#caption-pic').css({'background-image': imgCaption})
+        //alert(averageweight)
+        //alert(suggested_numplayers)
+        //data = list(records[0])
+        //alert(current_page)
+        if (current_page === 'gamerule'){
+        	nameEN_temp = change_nameEN(nameEN);
+			pageTitle = nameCN + nameEN_temp;
+			headerTitle = nameCN + nameEN_temp;
+			$("title").html(pageTitle);
+			$('#headerTitle').html(headerTitle);
+			//alert(headerTitle)
+        }
+        
+    } catch (e) { 
+    	alert(e)
+        //document.write(e.description); 
+    } finally { 
+    	
+    } 
+};
+
+var id = '';
+var type = '';
+var num = '';
+var textURL = 'http://180.76.244.130:3000/database/writeTextDB'
+
+
+
+
+function click_submit(textarea,num){
+    id = document.getElementById("gameid").value
+    type = document.getElementById("pageType").value
+    loc = document.getElementById("location").value   
+    text_content = document.getElementById("editor").innerHTML
+	//alert(id)
+	//alert(type)
+	//alert(text_content)
+	$.ajax({
+		url: textURL,
+		data:{
+			gameid:id,
+			pageType:type,
+			location:loc,
+			text: text_content
+		},
+		success:function(data){$("#alerts").value = data},
+		dataType:'json'
+	});
+}
